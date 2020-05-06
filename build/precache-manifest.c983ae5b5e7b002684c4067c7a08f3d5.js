dec972c13c6348fc3efb28045ef77101180fde9e
Olha Afanasieva <afanasievaolja@gmail.com>
2020-05-06 23:08:28 +0200

    files for heroku

---
+++ build/precache-manifest.c983ae5b5e7b002684c4067c7a08f3d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4f35bf5b9ed09ebf8cf2f5dfd64f50e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "106259ea43c0802222c7",
+    "url": "./static/css/main.7d4f7b32.chunk.css"
+  },
+  {
+    "revision": "29adee758a77f01becd0",
+    "url": "./static/js/2.d9d5ab20.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.d9d5ab20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "106259ea43c0802222c7",
+    "url": "./static/js/main.3f44c8ef.chunk.js"
+  },
+  {
+    "revision": "1e8c7afc7bf36ad0e5f1",
+    "url": "./static/js/runtime-main.64664b1b.js"
+  }
+]);
