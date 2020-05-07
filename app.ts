5ed7ebb26c1597b238b3cb68133c79d9d41cd6e2
Olha Afanasieva <afanasievaolja@gmail.com>
2020-05-07 13:11:53 +0200

    add app.listen

--- app.ts
+++ app.ts
@@ -48,5 +48,8 @@ app.use(function(err, req, res, next) {
   })
 })
 
-app.listen(3000)
-console.log('API running at http://localhost:3000')
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
+// app.listen(3000)
+// console.log('API running at http://localhost:3000')
